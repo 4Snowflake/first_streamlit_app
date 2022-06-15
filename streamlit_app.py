b21889639b91dfb2341a45c6a63f09b9d8711cc9
4Snowflake <107522447+4Snowflake@users.noreply.github.com>
2022-06-15 16:05:20 +0900

    change url

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,8 @@ streamlit.dataframe(fruits_to_show)
 
 #new api response
 import requests
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json())
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
