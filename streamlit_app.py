c82f018faea3fad37c5215ed86e46cdd8fdd030c
4Snowflake <107522447+4Snowflake@users.noreply.github.com>
2022-06-15 11:30:39 +0900

    Update streamlit_app.py
    Avocado and test inserted

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,5 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list =my_fruit_list.set_index('Fruit')
-streamlit.multiselect("pick one:",list(my_fruit_list.index))
+streamlit.multiselect("pick one:",list(my_fruit_list.index),['Avocado','TEST2'])
 streamlit.dataframe(my_fruit_list)
